44be1a60ab69f03abf1b2a2d8df119d8e7001f30
Philip Dobrynin <dobrynin.ph@yandex.ru>
2018-09-20 18:02:11 +0300

    Initial Commit

---
+++ src/MainClass.java
@@ -0,0 +1,37 @@
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class MainClass {
+  public static void main(String[] args) throws IOException {
+
+    File file = new File("E:\\Программы\\Обучение\\ТЗ_01.txt");
+    Scanner scanner = new Scanner(file);
+
+    List<String> list = new ArrayList<>();
+    while (scanner.hasNextLine()) {
+      String[] arrayTime = scanner.nextLine().split(" ");
+      for (int i = 0; i < arrayTime.length; i++) {
+        list.add(arrayTime[i].toLowerCase());
+      }
+    }
+    scanner.close();
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    String name = reader.readLine();
+
+    int wordCount = 0;
+    for (int i = 0; i < list.size(); i++) {
+      if (list.get(i).equals(name)) {
+        wordCount++;
+      }
+    }
+    printCount(wordCount);
+  }
+
+  public static void printCount(int x) {
+    System.out.println(x);
+  }
+}
+
