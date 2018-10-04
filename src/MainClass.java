d816aa6b9bf2ff1c36db8bca3db085a7d3986729
Philip Dobrynin <dobrynin.ph@yandex.ru>
2018-10-04 13:39:11 +0300

    fix gitignore

--- src/MainClass.java
+++ src/MainClass.java
@@ -7,27 +7,30 @@ import java.util.Scanner;
 public class MainClass {
   public static void main(String[] args) throws IOException {
 
-    File file = new File("E:\\Программы\\Обучение\\new 1.txt");
-    Scanner scanner = new Scanner(file);
-
-    List<String> list = new ArrayList<>();
-    while (scanner.hasNextLine()) {
-      String[] arrayTime = scanner.nextLine().split(" ");
-      for (int i = 0; i < arrayTime.length; i++) {
-        list.add(arrayTime[i]);
-      }
-    }
-    scanner.close();
     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-    String name = reader.readLine();
+    try {
+      File file = new File("new1.txt");
+      Scanner scanner = new Scanner(file);
 
-    int wordCount = 0;
-    for (int i = 0; i < list.size(); i++) {
-      if (list.get(i).equals(name)) {
-        wordCount++;
+      List<String> list = new ArrayList<>();
+      while (scanner.hasNextLine()) {
+        String[] arrayTime = scanner.nextLine().toLowerCase().split(" ");
+        for (int i = 0; i < arrayTime.length; i++) {
+          list.add(arrayTime[i]);
+        }
       }
+      scanner.close();
+      String name = reader.readLine().toLowerCase();
+      int wordCount = 0;
+      for (int i = 0; i < list.size(); i++) {
+        if (list.get(i).equals(name)) {
+          wordCount++;
+        }
+      }
+      printCount(wordCount);
+    } catch (FileNotFoundException e) {
+      System.out.println("Pathfile is not found");
     }
-    printCount(wordCount);
   }
 
   public static void printCount(int x) {
